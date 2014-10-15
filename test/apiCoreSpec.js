955ca6406c3f9f610a6a37a781034f90198df06a
license2e <ilya@shindyapin.com>
2014-10-15 00:01:11 -0400

    debug
    Signed-off-by: license2e <ilya@shindyapin.com>

---
+++ test/apiCoreSpec.js
@@ -0,0 +1,17 @@
+var assert = require('assert'),
+  ApiCore = require('..').ApiCore,
+  Intercom = require('facet-intercom');
+
+var appOptions = {
+  intercom: new Intercom
+};
+
+// var apiCore = new ApiCore(appOptions);
+
+describe('ApiCore', function() {
+
+  describe('#setupRouterManifest()', function(done) {
+    
+  });
+  
+});
